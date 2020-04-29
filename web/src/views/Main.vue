0fa8d21fb91ed0f048124cc5be769d6d5b74c935
minkukjo <strike0115@naver.com>
2020-04-29 23:39:25 +0900

    Axios 추가

--- web/src/views/Main.vue
+++ web/src/views/Main.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="main">
-    <h1>어서오셔들</h1>
+    <h1>{{ info }}</h1>
   </div>
 </template>
+
+<script>
+  export default {
+    el: '#main',
+    data() {
+      return {
+        info: null
+      }
+    },
+    mounted() {
+      axios
+        .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(response => (this.info = response))
+    }
+  }
+</script>
