b30194c3bc7189090d82544b4ddb332653b36275
minkukjo <strike0115@naver.com>
2020-05-13 22:39:51 +0900

    GCP 배포 스크립트 생성, 실제 프로덕션 배포

--- app/handler.go
+++ app/handler.go
@@ -27,7 +27,7 @@ var (
 			AuthURL:  "https://kauth.kakao.com/oauth/authorize",
 			TokenURL: "https://kauth.kakao.com/oauth/token",
 		},
-		RedirectURL: "http://localhost:9090/oauth/authorize",
+		RedirectURL: "http://34.67.130.46:9090/oauth/authorize",
 		Scopes:      nil,
 	}
 )
